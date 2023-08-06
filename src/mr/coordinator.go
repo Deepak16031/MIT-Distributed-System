a50401c3ac5ea15c7fc86a11822ae565bd2fbe79
deepak <deepak@deepaks-MacBook-Pro.local>
2023-08-06 08:27:10 +0530

    Implemented Coordinator Done function 1. Default value doesnt work, explicity set while declaring the coordinator struct. 2. Some code refactor.

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -11,10 +11,11 @@ import (
 
 type Coordinator struct {
 	// Your definitions here.
-	files []string
-	state []string
-	reduce []string
+	files     []string
+	state     []string
+	reduce    []string
 	partition int
+	ch        chan string
 }
 
 // Your code here -- RPC handlers for the worker to call.
@@ -24,7 +25,7 @@ func (c *Coordinator) Implementation(args *WorkerArgs, reply *CoordindatorArgs) 
 	fmt.Println(c.files)
 	fmt.Println(c.state)
 	fmt.Println(c.reduce)
-	for i:= 0; i< len(c.state) ;i++ {
+	for i := 0; i < len(c.state); i++ {
 		if c.state[i] == "idle" {
 			c.state[i] = "in_progress"
 			reply.Task = "Map"
@@ -34,7 +35,7 @@ func (c *Coordinator) Implementation(args *WorkerArgs, reply *CoordindatorArgs) 
 			return nil
 		}
 	}
-	for i:= 0 ; i < len(c.reduce) ; i++ {
+	for i := 0; i < len(c.reduce); i++ {
 		if c.reduce[i] == "idle" {
 			c.reduce[i] = "in_progress"
 			reply.Files = make([]string, len(c.files))
@@ -45,7 +46,8 @@ func (c *Coordinator) Implementation(args *WorkerArgs, reply *CoordindatorArgs) 
 			return nil
 		}
 	}
-	if (flag == 0) {
+	if flag == 0 {
+		c.ch <- "I am done too"
 		reply.Task = "Done"
 	}
 	return nil
@@ -54,15 +56,12 @@ func (c *Coordinator) Implementation(args *WorkerArgs, reply *CoordindatorArgs) 
 // an example RPC handler.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
 
-//
 // start a thread that listens for RPCs from worker.go
-//
 func (c *Coordinator) server() {
 	rpc.Register(c)
 	rpc.HandleHTTP()
@@ -76,36 +75,36 @@ func (c *Coordinator) server() {
 	go http.Serve(l, nil)
 }
 
-//
 // main/mrcoordinator.go calls Done() periodically to find out
 // if the entire job has finished.
-//
 func (c *Coordinator) Done() bool {
 	ret := false
 
 	// Your code here.
-
+	select {
+	case <-c.ch:
+		ret = true
+	}
 	return ret
 }
 
-//
 // create a Coordinator.
 // main/mrcoordinator.go calls this function.
 // nReduce is the number of reduce tasks to use.
-//
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
 	c := Coordinator{}
-	
+
 	// Your code here.
 	c.files = files
 	c.state = make([]string, len(files))
 	c.reduce = make([]string, len(files))
+	c.ch = make(chan string)
 	c.partition = nReduce
-	for i:= 0 ;i < len(files); i++ {
+	for i := 0; i < len(files); i++ {
 		c.state[i] = "idle"
-		c.reduce[i] ="idle"
+		c.reduce[i] = "idle"
 	}
-	
+
 	c.server()
 	return &c
 }
